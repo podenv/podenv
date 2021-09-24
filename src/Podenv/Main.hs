fdb730c48042ec66d8fa9d8ee97fecbefa913001
Jens Petersen <juhpetersen@gmail.com>
2021-09-24 17:02:02 +0800

    Main: redundant import of Podenv.Env

--- src/Podenv/Main.hs
+++ src/Podenv/Main.hs
@@ -29,7 +29,6 @@ import Podenv.Build (BuildEnv (beInfos, beName, beUpdate))
 import qualified Podenv.Build
 import qualified Podenv.Config
 import Podenv.Dhall
-import qualified Podenv.Env
 import Podenv.Prelude
 import Podenv.Runtime (Context, RuntimeEnv (..))
 import qualified Podenv.Runtime
