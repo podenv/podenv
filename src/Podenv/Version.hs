b545fe67d5e65cf70bfb932a6905151d4364e193
Tristan Cacqueray <tdecacqu@redhat.com>
2021-10-15 18:08:53 +0000

    [nix] inject git version in the build environment

--- src/Podenv/Version.hs
+++ src/Podenv/Version.hs
@@ -4,10 +4,15 @@
 module Podenv.Version (version) where
 
 import Development.GitRev (gitDirty, gitHash)
+import Language.Haskell.TH.Env (envQ)
 
 version :: String
-version = $(gitHash) <> dirty
+version = case fromEnv of
+  Just v -> v
+  Nothing -> $(gitHash) <> dirty
   where
+    fromEnv :: Maybe String
+    fromEnv = $$(envQ "PODENV_COMMIT")
     dirty
       | $(gitDirty) = "-dirty"
       | otherwise = ""
